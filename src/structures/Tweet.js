69288afae73e2b80e208404aa56b5e165cebecf5
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-03-20 18:17:00 -0700

    Added Tweet#reply(message)

--- src/structures/Tweet.js
+++ src/structures/Tweet.js
@@ -67,6 +67,22 @@ class Tweet {
     })
   }
   
+  reply(message) {
+    return new Promise((resolve, reject) => {
+      if (typeof message != 'string') reject(new Error('The message to reply with must be a string!'));
+      // message = `@${this.author.username} ${message}`;
+      post(this.auth, `statuses/update`, {status: message, in_reply_to_status_id: this.id})
+        .then(response => {
+          let body = JSON.parse(response.body);
+          if (response.statusCode != 200 && body.errors) return reject(new Error(`API returned error ${body.errors[0].code}: ${body.errors[0].message}`));
+          return resolve(new Tweet(this.auth, body));
+        })
+        .catch(e => {
+          return reject(e);
+        })
+    })
+  }
+  
   delete() {
     return new Promise((resolve, reject) => {
       post(this.auth, `statuses/destroy/${this.id}`)
